4a2ea09764cb4777f117a298dcaadaee61014a5a
Joshua-Dela-Pena <richmelldelapena@gmail.com>
2022-01-12 16:18:07 -0600

    Created the put and delete functions for the backend

--- musiclibrary/music/views.py
+++ musiclibrary/music/views.py
@@ -23,3 +23,22 @@ class SongList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class EditSong(APIView):    
+    def put(self, request, pk):
+        song = self.get_by_id(pk)
+        serializer = SongSerializer(song, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        song = self.get_by_id(pk)
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    def get_by_id(self, pk):
+            try:
+                return Song.objects.get(pk=pk)
+            except Song.DoesNotExist:
+                raise Http404
