dfa71dce0065fa939730bf11bbd2f0c4f05087ce
Joshua-Dela-Pena <richmelldelapena@gmail.com>
2022-01-13 15:13:35 -0600

    Added an import to make my except raise Http 404 works as it should

--- musiclibrary/music/views.py
+++ musiclibrary/music/views.py
@@ -5,7 +5,7 @@ from .serializers import SongSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import serializers, status
-
+from django.http import Http404
 
 # Create your views here.
 
